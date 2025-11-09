d58cae45449821ef7ead548adeda4be8a1994982
pinghoyk <pinghoyk009@gmail.com>
2025-11-09 17:19:42 +0500

    Храню подключение к бд

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,2 +1,11 @@
-// как работать с бд, запросы и создание таблиц
 package storage
+
+import "database/sql"
+
+type Storage struct {
+	db *sql.DB
+}
+
+func New(db *sql.DB) *Storage {
+	return &Storage{db: db}
+}
