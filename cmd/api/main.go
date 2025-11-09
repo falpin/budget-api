7cf2da4289e31e860ce33d1ca04b339c3f428a0f
pinghoyk <pinghoyk009@gmail.com>
2025-11-09 14:18:11 +0500

    Получаем переменную из .env

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -9,6 +9,11 @@ func main() {
 		log.Println("Файл .env не найден!")
 	}
 
+	dbPath := os.Getenv("DB_PATH")
+	if dbPath == "" {
+		dbPath = "budget-data/budget.db"
+		log.Printf("DB_PATH не задан - используем значение по умолчанию: %s", dbPath)
+	}
 }
 
 // пока все пишу тут, потом надо разделить файлы и кинут в internal
